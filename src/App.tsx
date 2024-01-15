381d736f72605ec7beb5f1fb0fa5e745ce83e9fe
Austin Riley <AustinRiley91@gmail.com>
2024-01-15 12:35:16 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { MQ } from "./styles/breakpoints";
+import ROUTES from "./router/routes";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import styled, {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    overflow-x: hidden;
+  }
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  height: 100vh;
+  ${MQ.md} {
+    align-items: center;
+    justify-content: center;
+    height: inherit;
+  }
+`;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <AppContainer>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+        </Routes>
+      </AppContainer>
+    </BrowserRouter>
+  );
+}
+
+export default App;
